6cd992450862e3737e7c4a2ebf9cbffe18ccc871
Elvis Miranda <elvis@bertonisolutions.com>
2025-02-25 05:43:45 -0400

    Add Inspections table

---
+++ TuryBus/Tables/Inspections.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Inspections (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    bus_id INT NOT NULL,
+    [date] DATE NOT NULL, -- Fecha de inspeccion
+    diagnosis NVARCHAR(255) NOT NULL, -- Diagnóstico
+    --FOREIGN KEY (bus_id) REFERENCES Busses(id)
+);
