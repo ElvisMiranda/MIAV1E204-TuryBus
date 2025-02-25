ac13eb3a3107e6a0ee481d93f813cfb1c12b34a3
Elvis Miranda <elvis@bertonisolutions.com>
2025-02-25 05:50:08 -0400

    Move table definitions to its own file

--- TuryBus/Tables/Passengers.sql
+++ TuryBus/Tables/Passengers.sql
@@ -1,7 +1,6 @@
-﻿CREATE TABLE Drivers (
+﻿CREATE TABLE Passengers (
     id INT PRIMARY KEY IDENTITY(1,1),
     dni NVARCHAR(20) UNIQUE NOT NULL,
     name NVARCHAR(100) NOT NULL,
-    phone NVARCHAR(20) NOT NULL,
-    address NVARCHAR(255) NOT NULL
+    phone NVARCHAR(20) NOT NULL
 );
---
+++ TuryBus/Tables/Routes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Routes (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(100) NOT NULL,
+    description NVARCHAR(100) NOT NULL, -- Descripción de la ruta
+	distance DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
