1cb0af177ea9828e12548423d918e773a3f96adc
Karenptv15 <kptorricov@soe.uagrm.edu.bo>
2025-02-24 22:16:31 -0400

    Update Drivers.sql

--- TuryBus/Tables/Drivers.sql
+++ TuryBus/Tables/Drivers.sql
@@ -5,7 +5,6 @@
     phone NVARCHAR(20) NOT NULL,
     address NVARCHAR(255) NOT NULL
 );
--- Table of tourist routes
 CREATE TABLE Routes (
     id INT PRIMARY KEY IDENTITY(1,1),
     name NVARCHAR(100) NOT NULL,
@@ -13,7 +12,7 @@ CREATE TABLE Routes (
 	distance DECIMAL(10,2) NOT NULL,
     price DECIMAL(10,2) NOT NULL
 );
--- Table of passengers
+
 CREATE TABLE Passengers (
     id INT PRIMARY KEY IDENTITY(1,1),
     dni NVARCHAR(20) UNIQUE NOT NULL,
