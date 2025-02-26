3cb1b8ba6632b66cc1cb0cb7776e537af27e0070
khuacota <karem.huacota@gmail.com>
2025-02-25 22:03:28 -0400

    Added drivers data

--- TuryBus/Scripts/Script.PostDeployment1.sql
+++ TuryBus/Scripts/Script.PostDeployment1.sql
@@ -1,2 +1,8 @@
 ﻿INSERT INTO Activities (description) VALUES ('Visita');
 INSERT INTO Activities (description) VALUES ('Comida');
+
+INSERT INTO Drivers (dni, name, phone, address) VALUES ('123456', 'Gustavo Lavadenz', '73499222', 'Av. San Martin');
+INSERT INTO Drivers (dni, name, phone, address) VALUES ('123457', 'Rafael Sanchez', '73455222', 'Av. America');
+INSERT INTO Drivers (dni, name, phone, address) VALUES ('123458', 'Carlos Martinez', '73477123', 'Av. Ayacucho');
+INSERT INTO Drivers (dni, name, phone, address) VALUES ('123459', 'Dante Reynaga', '72255163', 'Av. Blanco Galindo');
+INSERT INTO Drivers (dni, name, phone, address) VALUES ('123460', 'Alex Arevalo', '72288883', 'Av. Colon');
