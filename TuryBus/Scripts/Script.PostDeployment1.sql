ad46b293086c01e15c9e2d9271b95d3c69a9d811
unknown <jessical8762@gmail.com>
2025-02-25 22:16:29 -0400

    insert passengers

--- TuryBus/Scripts/Script.PostDeployment1.sql
+++ TuryBus/Scripts/Script.PostDeployment1.sql
@@ -1,15 +1,20 @@
 ﻿INSERT INTO Activities (description) VALUES ('Visita');
 INSERT INTO Activities (description) VALUES ('Comida');
 
-<<<<<<< HEAD
+
 INSERT INTO Drivers (dni, name, phone, address) VALUES ('123456', 'Gustavo Lavadenz', '73499222', 'Av. San Martin');
 INSERT INTO Drivers (dni, name, phone, address) VALUES ('123457', 'Rafael Sanchez', '73455222', 'Av. America');
 INSERT INTO Drivers (dni, name, phone, address) VALUES ('123458', 'Carlos Martinez', '73477123', 'Av. Ayacucho');
 INSERT INTO Drivers (dni, name, phone, address) VALUES ('123459', 'Dante Reynaga', '72255163', 'Av. Blanco Galindo');
 INSERT INTO Drivers (dni, name, phone, address) VALUES ('123460', 'Alex Arevalo', '72288883', 'Av. Colon');
-=======
+--=======
 INSERT INTO Routes (name, description, distance, price) VALUES 
 ('Ruta del Sol', 'Explora las hermosas playas', 100.50, 150.00),
 ('Aventura en la Montaña', 'Un recorrido emocionante por las montañas', 80.75, 120.00),
 ('Tour Gastronómico', 'Descubre los sabores locales', 50.30, 100.00);
->>>>>>> aff1f2a80dd1a795bd47208beda8afade6bc2c5a
+
+INSERT INTO Passengers (dni, name, phone) VALUES
+('56789012D', 'Jorge Pérez', '852963741'),
+('67890123E', 'María Fernández', '963852741'),
+('78901234F', 'José Ramírez', '741852963'),
+('89012345G', 'Ana Morales', '852963147');
