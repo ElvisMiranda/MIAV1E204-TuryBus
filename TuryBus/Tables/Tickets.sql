f55ba6f92955d146d9597991553c7b83fc58c5ee
unknown <jessical8762@gmail.com>
2025-02-25 21:22:45 -0400

    se adiciono la tabla ticket

---
+++ TuryBus/Tables/Tickets.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Tickets (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    passenger_id INT NOT NULL,
+    service_id INT NOT NULL,
+    travel_date DATE NOT NULL, -- Fecha de viaje
+    FOREIGN KEY (passenger_id) REFERENCES Passengers(id),
+    FOREIGN KEY (service_id) REFERENCES DailyServices(id)
+);
