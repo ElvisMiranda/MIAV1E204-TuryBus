aff1f2a80dd1a795bd47208beda8afade6bc2c5a
unknown <jessical8762@gmail.com>
2025-02-25 21:58:53 -0400

    insertar datos

--- TuryBus/Scripts/Script.PostDeployment1.sql
+++ TuryBus/Scripts/Script.PostDeployment1.sql
@@ -1,2 +1,7 @@
 ﻿INSERT INTO Activities (description) VALUES ('Visita');
 INSERT INTO Activities (description) VALUES ('Comida');
+
+INSERT INTO Routes (name, description, distance, price) VALUES 
+('Ruta del Sol', 'Explora las hermosas playas', 100.50, 150.00),
+('Aventura en la Montaña', 'Un recorrido emocionante por las montañas', 80.75, 120.00),
+('Tour Gastronómico', 'Descubre los sabores locales', 50.30, 100.00);
