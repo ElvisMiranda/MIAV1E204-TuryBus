81d439c77f9105d53729f24266134873428c2649
Elvis Miranda <elvis.miranda@n5now.com>
2025-02-25 20:57:48 -0400

    Update Inspections Table

--- TuryBus/Tables/Inspections.sql
+++ TuryBus/Tables/Inspections.sql
@@ -3,5 +3,5 @@
     bus_id INT NOT NULL,
     [date] DATE NOT NULL, -- Fecha de inspeccion
     diagnosis NVARCHAR(255) NOT NULL, -- Diagnóstico
-    --FOREIGN KEY (bus_id) REFERENCES Busses(id)
+    FOREIGN KEY (bus_id) REFERENCES Busses(id)
 );
