e9b1ef6703a8ba7abaa493b013b0700218620b7d
Ivan Mamani <imamanic@soe.uagrm.edu.bo>
2025-03-03 23:42:49 -0800

    OLTP: Adjust UNIQUE keys

--- TuryBusOLTP/Schema/Tables/Passengers.sql
+++ TuryBusOLTP/Schema/Tables/Passengers.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE Passengers (
     id INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_passengers PRIMARY KEY,
-    dni NVARCHAR(20) UNIQUE NOT NULL,
+    dni NVARCHAR(20) NOT NULL,
     name NVARCHAR(100) NOT NULL,
     phone NVARCHAR(20) NOT NULL
 );
 GO
+
+ALTER TABLE Passengers ADD CONSTRAINT UQ_passengers_dni UNIQUE (dni);
