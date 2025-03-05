9163bd8d3c2305aad1b8215ca7d4b82047433ba1
Iván Mamani <30558501+mc-ivan@users.noreply.github.com>
2025-03-04 20:05:38 -0800

    DW: Add factStops and FactRepairs tables (#9)
    Co-authored-by: Ivan Mamani <imamanic@soe.uagrm.edu.bo>

---
+++ TuryBusDW/Schema/Tables/FactRepairs.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE FactRepairs (
+    repair_id INT PRIMARY KEY IDENTITY(1,1),
+    bus_sk INT NOT NULL,
+    date_Key INT NOT NULL,
+    repair_sk INT NOT NULL,
+    total_time_spent INT NOT NULL, -- Minutos
+    comments NVARCHAR(500)
+);
+GO
+
+ALTER TABLE FactRepairs ADD CONSTRAINT FK_factRepairs_dimBusses FOREIGN KEY (bus_sk) REFERENCES DimBusses(bus_sk)
+GO
+
+ALTER TABLE FactRepairs ADD CONSTRAINT FK_factRepairs_dimDate FOREIGN KEY (date_Key) REFERENCES DimDate(date_Key)
+GO
+
+ALTER TABLE FactRepairs ADD CONSTRAINT FK_factRepairs_dimRepairs FOREIGN KEY (repair_sk) REFERENCES DimRepairs(repair_sk)
+GO
