f0640186453ecb833e473bd6b87a47047d4105f8
@ElvisMiranda <elvis.miranda@live.com>
2025-03-05 10:32:31 -0400

    Add DimRepairs & FactServices tables (#16)

---
+++ TuryBusDW/Schema/Tables/DimRepairs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE DimRepairs (
+    repair_sk INT IDENTITY(1,1) CONSTRAINT PK_dimRepairs PRIMARY KEY,
+    repair_id INT NOT NULL,
+    type NVARCHAR(100) NOT NULL, -- Tipo de reparación
+    time_spent INT NOT NULL, -- Tiempo empleado en minutos
+    comment NVARCHAR(255)
+);
