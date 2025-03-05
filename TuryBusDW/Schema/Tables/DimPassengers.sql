a3d39891d0eb7189399fb5f0558219202e0496d8
YesikaLunaR <jessical8762@gmail.com>
2025-03-05 00:10:54 -0400

    Adicionar las tablas DimPassenger y DimPlaces (#13)
    Co-authored-by: Iván Mamani <30558501+mc-ivan@users.noreply.github.com>

---
+++ TuryBusDW/Schema/Tables/DimPassengers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE DimPassengers (
+    passenger_sk INT IDENTITY(1,1) CONSTRAINT PK_dimPassengers PRIMARY KEY,
+    passenger_id INT NOT NULL,
+    dni NVARCHAR(20) NOT NULL UNIQUE,
+    name NVARCHAR(255) NOT NULL,
+    phone NVARCHAR(20) NOT NULL
+);
---
+++ TuryBusDW/Schema/Tables/DimPlaces.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE DimPlaces (
+    place_sk INT IDENTITY(1,1) CONSTRAINT PK_dimPlaces PRIMARY KEY,
+    place_id INT NOT NULL,
+    name NVARCHAR(255) NOT NULL
+);
