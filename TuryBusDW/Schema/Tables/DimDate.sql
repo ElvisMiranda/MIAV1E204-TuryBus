868d602378313a69e87fab5b67375e5acc17ef99
khuacota <43738643+khuacota@users.noreply.github.com>
2025-03-05 10:18:30 -0400

    Added DimDate and DimRoutes (#15)

---
+++ TuryBusDW/Schema/Tables/DimDate.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE DimDate (
+    date_Key int NOT NULL CONSTRAINT PK_DimDate PRIMARY KEY,
+    full_date DATE NOT NULL,
+    year INT NOT NULL,
+    month INT NOT NULL,
+    day INT NOT NULL
+);
+
---
+++ TuryBusDW/Schema/Tables/DimRoutes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE DimRoutes (
+    route_sk INT IDENTITY(1,1) CONSTRAINT PK_dimRoutes PRIMARY KEY,
+    route_id INT NOT NULL,
+    name NVARCHAR(255) NOT NULL,
+    description NVARCHAR(100) NOT NULL, -- Descripción de la ruta
+    distance DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
+
