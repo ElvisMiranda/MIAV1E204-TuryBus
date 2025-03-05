05338ffde26edaf94481c5ce681c55eed28ae5df
Elvis Miranda <elvis.miranda@live.com>
2025-03-05 10:28:46 -0400

    Add DimRepairs & FactServices tables

---
+++ TuryBusDW/Schema/Tables/FactServices.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE FactServices (
+    service_id INT IDENTITY(1,1) CONSTRAINT PK_dimServices PRIMARY KEY,
+    route_sk INT NOT NULL,
+    bus_sk INT NOT NULL,
+    driver_sk INT NOT NULL,
+    passenger_sk INT NOT NULL,
+    date_Key INT NOT NULL,
+    departure_time TIME NOT NULL,
+    arrival_time TIME NOT NULL,
+    total_passengers INT NOT NULL
+);
