e6acbe7abb1944782a24f42047aee74d10dfbc0b
Karenptv15 <kptorricov@soe.uagrm.edu.bo>
2025-03-09 19:39:05 -0400

    Update Activities.sql

--- TuryBusOLTP/Schema/Tables/Activities.sql
+++ TuryBusOLTP/Schema/Tables/Activities.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE Activities (
     id INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_activities PRIMARY KEY,
     description NVARCHAR(100), -- Actividad a realizar
+    [rowversion]  [timestamp]         NOT NULL
 );
 
