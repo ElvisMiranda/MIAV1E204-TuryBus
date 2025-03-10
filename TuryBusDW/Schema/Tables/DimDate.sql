c6fa911b5b4532d39f7f54394cb42ac9a1a6bf60
WINDOWS10 <WINDOWS10@DESKTOP-3DVFDPI>
2025-03-09 20:28:35 -0400

    agregado dimdate y packageconfig

--- TuryBusDW/Schema/Tables/DimDate.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE DimDate (
-    date_Key int NOT NULL CONSTRAINT PK_DimDate PRIMARY KEY,
-    full_date DATE NOT NULL,
-    year INT NOT NULL,
-    month INT NOT NULL,
-    day INT NOT NULL
-);
-
