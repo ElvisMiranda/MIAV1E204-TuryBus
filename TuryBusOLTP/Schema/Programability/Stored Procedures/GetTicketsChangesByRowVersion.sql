a852574ea15cba2179f0030486cb7e118488827b
khuacota <43738643+khuacota@users.noreply.github.com>
2025-03-10 01:43:53 -0400

    Added stored procedures (#36)

---
+++ TuryBusOLTP/Schema/Programability/Stored Procedures/GetTicketsChangesByRowVersion.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetTicketsChangesByRowVersion]
+(
+	@startRow BIGINT,
+	@endRow BIGINT
+)
+AS
+BEGIN
+	  SELECT t.id
+		    ,t.travel_date
+		    ,p.dni
+		    ,p.name
+		    ,p.phone
+	  FROM Tickets t
+	  INNER JOIN Passengers p ON t.passenger_id = p.id
+	  WHERE (t.rowversion > CONVERT(ROWVERSION,@startRow) 
+	  AND t.rowversion <= CONVERT(ROWVERSION,@endRow))
+	  OR (p.rowversion > CONVERT(ROWVERSION,@startRow)
+	  AND p.rowversion <= CONVERT(ROWVERSION,@endRow))
+END
+GO
+
