d71f4fd519182efd4b3eb65e9c86afbf58a9417f
Ivan Mamani <imamanic@soe.uagrm.edu.bo>
2025-03-11 02:03:43 -0700

    Revert "Fix table definition and add Stop ETL tasks (#49)"
    This reverts commit c27f2162738536a90e7aef4f0aef2c890495dccc.

--- TuryBusDW/Schema/Tables/staging/Stop.sql
+++ TuryBusDW/Schema/Tables/staging/Stop.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE staging.stop (
     StopSK INT NOT NULL,
-    PlaceName NVARCHAR(100) NOT NULL,
-    ActivityDescription NVARCHAR(100) NOT NULL,
+    PlaceName VARCHAR(100),
+    ActivityDescription VARCHAR(50) NOT NULL,
     ArrivalTime DATETIME NOT NULL,
     StopDuration INT DEFAULT 0
 );
