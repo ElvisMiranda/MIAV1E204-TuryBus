dcafe5cdc560a303eb58724427832804644d8556
Elvis Miranda <elvis.miranda@live.com>
2025-03-11 00:43:37 -0400

    Add Reporting Sevices Project

---
+++ TuryBusDW/Schema/Programmability/Stored Procedures/sp_GetAvgPassengersByRoute.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetAvgPassengersByRoute]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT 
+		r.RouteID AS route_id
+		, r.name AS route_name
+	   , COUNT(fs.ServiceID) / COUNT(DISTINCT fs.ServiceID) AS avg_passengers
+	FROM FactServices fs
+	JOIN DimRoute r ON fs.RouteSK = r.RouteSK
+	GROUP BY r.RouteID, r.Name;
+END;
---
+++ TuryBusDW/Schema/Programmability/Stored Procedures/sp_GetTotalHoursByUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetTotalHoursByUser]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	SELECT 
+        fs.PassengerSK AS passenger,
+        SUM(DATEDIFF(MINUTE, fs.DepartureTime, fs.ArrivalTime)) / 60 AS total_hours
+    FROM FactServices fs
+    JOIN DimPassenger p ON fs.PassengerSK = p.PassengerSK
+    GROUP BY fs.PassengerSK;
+END
