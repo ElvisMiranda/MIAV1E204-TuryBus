10f55cc075574fe8d28176d1e4e295e9109034e3
Iván Mamani <30558501+mc-ivan@users.noreply.github.com>
2025-03-10 20:52:49 -0700

    Yesika dw3 (#43)
    * adicionar merge routes
    
    * adicionar merge routes
    
    ---------
    
    Co-authored-by: Yesika Luna <jessical8762@gmail.com>

---
+++ TuryBusDW/Schema/Programmability/Stored Procedures/DW_MergeDimRoute.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimRoute]
+AS
+BEGIN
+    UPDATE ds
+    SET 
+     ds.[Name]=sc.[Name],
+      ds.[Description]=sc.[Description],
+      ds.Distance=sc.Distance,
+      ds.Price=sc.Price
+
+
+    FROM [dbo].[DimRoute] ds
+    INNER JOIN [staging].[Route] sc ON ds.RouteSK = sc.RouteSK;
+END
+GO
