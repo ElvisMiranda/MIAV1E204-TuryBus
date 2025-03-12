27500f365a00cdda622a5647e31be748d8a5db74
YesikaLunaR <jessical8762@gmail.com>
2025-03-11 21:11:49 -0400

    adicionar cambios DIM passenger (#55)

---
+++ TuryBusDW/Schema/Programmability/Stored Procedures/DW_MergeDimPassenger.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimPassenger]
+AS
+BEGIN
+
+	UPDATE ds
+		SET 
+			ds.[dni] = s.[dni]
+			, ds.[name] = s.[name]
+			, ds.[phone] = s.[phone]
+
+	FROM [dbo].[DimPassenger] ds
+	INNER JOIN [staging].[Passenger] s ON ds.PassengerSK = s.PassengerSK
+
+END
+GO
